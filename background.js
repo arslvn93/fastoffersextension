4520f9857de46bb26d70047f4e4b64c51cda3df5
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2022-03-23 13:24:21 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -7,16 +7,29 @@ chrome.runtime.onMessage.addListener(function (msg, sender, response) {
                 "Content-Type": "application/json"
             }
         }).then(response => response.json()).then(data => {
-              console.log(JSON.stringify(data));
+            console.log(JSON.stringify(data));
         });
 
         chrome.tabs.create({url: msg.url, active: true}, function (tab) {});
     }
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.url && (!changeInfo.url.includes("https://forms.ltd"))) {
         chrome.tabs.sendMessage(tabId, {from: 'background', subject: 'changedUrl'});
     }
 });
 
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason == "install") {
+        login();
+    }
+});
+
+chrome.action.onClicked.addListener((tab) => {
+    login();
+});
+
+function login() {
+    chrome.tabs.create({url: "https://forms.ltd/Booster", active: true}, function (tab) {});
+}
